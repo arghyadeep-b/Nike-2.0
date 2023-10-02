d58bd53dea39fe5790b512eb84439f50ea1d67f3
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-10-02 19:35:08 +0530

    Special Offer section ready

--- nike-landing-page/src/components/Button.jsx
+++ nike-landing-page/src/components/Button.jsx
@@ -1,6 +1,18 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${
+        backgroundColor ? `bg-${backgroundColor}` : "bg-coral-red"
+      } ${
+        borderColor ? `border-${borderColor}` : "border-coral-red"
+      } rounded-full ${textColor ? `border-${textColor}` : "text-white"}`}
+    >
       {label}
       {iconURL && (
         <img src={iconURL} alt={label} className="ml-2 rounded-full w-5 h-5" />
