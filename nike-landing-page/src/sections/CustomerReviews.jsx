4d4a0ff4f388cc72b9a217fca3559bd16c88b8aa
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-10-05 11:58:43 +0530

    Reviews section ready

--- nike-landing-page/src/sections/CustomerReviews.jsx
+++ nike-landing-page/src/sections/CustomerReviews.jsx
@@ -1,5 +1,26 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+
 const CustomerReviews = () => {
-  return <div></div>;
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red">Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col">
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} {...review} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReviews;
